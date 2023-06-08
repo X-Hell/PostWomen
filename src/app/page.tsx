6a0926767cf085e57248611f8fcb9ad8046457d8
Manish Shaw <monishofficial8@gmail.com>
2023-06-08 21:17:39 +0530

    Input field

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,270 @@
-import Image from 'next/image'
+"use client";
+import React from "react";
+import { useEffect, useState } from "react";
 
-export default function Home() {
+const KeyValueTemplate = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className="input-group my-2" data-key-value-pair>
+      <input type="text" data-key className="form-control" placeholder="Key" />
+      <input
+        type="text"
+        data-value
+        className="form-control"
+        placeholder="Value"
+      />
+      <button type="button" data-remove-btn className="btn btn-outline-danger">
+        Remove
+      </button>
+    </div>
+  );
+};
+
+const MyComponent = () => {
+  const [activeTab, setActiveTab] = useState("tabs-home");
+
+  const handleTabClick = (tabId) => {
+    setActiveTab(tabId);
+  };
+
+  return (
+    <div className="p-6">
+      <form data-form>
+        <div className="mb-4 flex items-center h-10">
+          <select className="py-2 px-3 border border-gray-300 rounded-l-md h-full">
+            <option value="GET" selected>
+              GET
+            </option>
+            <option value="POST">POST</option>
+            <option value="PUT">PUT</option>
+            <option value="PATCH">PATCH</option>
+            <option value="DELETE">DELETE</option>
+          </select>
+          <input
+            className="block w-full py-2 px-3 border border-gray-300 h-full"
+            type="url"
+            placeholder="https://example.com"
+          />
+
+          <button className="px-4 py-2 h-full border border-gray-500 rounded-r-md bg-blue-500 hover:bg-blue-700 text-white">
+            Send
+          </button>
+        </div>
+
+        <div className="mb-6">
+          <ul className="flex mb-5 list-none border-b-2 pl-0">
+            <li
+              className={`mr-4 ${
+                activeTab === "tabs-home" ? "border-b-2 border-blue-500" : ""
+              }`}
+            >
+              <button
+                className="px-4 py-2 font-medium text-sm uppercase text-neutral-500 hover:text-blue-500"
+                onClick={() => handleTabClick("tabs-home")}
+              >
+                Home
+              </button>
+            </li>
+            <li
+              className={`mr-4 ${
+                activeTab === "tabs-profile" ? "border-b-2 border-blue-500" : ""
+              }`}
+            >
+              <button
+                className="px-4 py-2 font-medium text-sm uppercase text-neutral-500 hover:text-blue-500"
+                onClick={() => handleTabClick("tabs-profile")}
+              >
+                Profile
+              </button>
+            </li>
+            <li
+              className={`mr-4 ${
+                activeTab === "tabs-messages"
+                  ? "border-b-2 border-blue-500"
+                  : ""
+              }`}
+            >
+              <button
+                className="px-4 py-2 font-medium text-sm uppercase text-neutral-500 hover:text-blue-500"
+                onClick={() => handleTabClick("tabs-messages")}
+              >
+                Messages
+              </button>
+            </li>
+            <li
+              className={`mr-4 ${
+                activeTab === "tabs-contact" ? "border-b-2 border-blue-500" : ""
+              }`}
+            >
+              <button
+                className="px-4 py-2 font-medium text-sm uppercase text-neutral-500 hover:text-blue-500"
+                onClick={() => handleTabClick("tabs-contact")}
+              >
+                Contact
+              </button>
+            </li>
+          </ul>
+
+          <div
+            id="tabs-home"
+            className={`hidden ${
+              activeTab === "tabs-home" ? "block" : "hidden"
+            }`}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            Tab 1 content
+          </div>
+          <div
+            id="tabs-profile"
+            className={`hidden ${
+              activeTab === "tabs-profile" ? "block" : "hidden"
+            }`}
+          >
+            Tab 2 content
+          </div>
+          <div
+            id="tabs-messages"
+            className={`hidden ${
+              activeTab === "tabs-messages" ? "block" : "hidden"
+            }`}
+          >
+            Tab 3 content
+          </div>
+          <div
+            id="tabs-contact"
+            className={`hidden ${
+              activeTab === "tabs-contact" ? "block" : "hidden"
+            }`}
+          >
+            Tab 4 content
+          </div>
+        </div>
+
+        <div className="tab-content p-3 border-t-0 border">
+          <div
+            className="tab-pane fade show active"
+            id="query-params"
+            role="tabpanel"
+            aria-labelledby="query-params-tab"
+          >
+            <div data-query-params></div>
+            <button
+              data-add-query-param-btn
+              className="mt-2 btn btn-outline-success"
+              type="button"
+            >
+              Add
+            </button>
+          </div>
+          <div
+            className="tab-pane fade"
+            id="request-headers"
+            role="tabpanel"
+            aria-labelledby="request-headers-tab"
+          >
+            <div data-request-headers></div>
+            <button
+              data-add-request-header-btn
+              className="mt-2 btn btn-outline-success"
+              type="button"
+            >
+              Add
+            </button>
+          </div>
+          <div
+            className="tab-pane fade"
+            id="json"
+            role="tabpanel"
+            aria-labelledby="json-tab"
+          >
+            <div
+              data-json-request-body
+              className="overflow-auto"
+              style={{ maxHeight: "200px" }}
+            ></div>
+          </div>
+        </div>
+      </form>
+
+      <div className="mt-5 hidden" data-response-section>
+        <h3>Response</h3>
+        <div className="flex my-2">
+          <div className="me-3">
+            Status: <span data-status></span>
+          </div>
+          <div className="me-3">
+            Time: <span data-time></span>ms
+          </div>
+          <div className="me-3">
+            Size: <span data-size></span>
+          </div>
+        </div>
+
+        <ul className="nav nav-tabs" role="tablist">
+          <li className="nav-item" role="presentation">
+            <button
+              className="nav-link active"
+              id="body-tab"
+              data-bs-toggle="tab"
+              data-bs-target="#body"
+              type="button"
+              role="tab"
+              aria-controls="body"
+              aria-selected="true"
+            >
+              Body
+            </button>
+          </li>
+          <li className="nav-item" role="presentation">
+            <button
+              className="nav-link"
+              id="response-headers-tab"
+              data-bs-toggle="tab"
+              data-bs-target="#response-headers"
+              type="button"
+              role="tab"
+              aria-controls="response-headers"
+              aria-selected="false"
+            >
+              Headers
+            </button>
+          </li>
+        </ul>
+
+        <div className="tab-content p-3 border-t-0 border">
+          <div
+            className="tab-pane fade show active"
+            id="body"
+            role="tabpanel"
+            aria-labelledby="body-tab"
+          >
+            <div
+              data-json-response-body
+              className="overflow-auto"
+              style={{ maxHeight: "200px" }}
+            ></div>
+          </div>
+          <div
+            className="tab-pane fade"
+            id="response-headers"
+            role="tabpanel"
+            aria-labelledby="response-headers-tab"
+          >
+            <div
+              style={{
+                display: "grid",
+                gridTemplateColumns: "auto 1fr",
+                gap: ".5rem 2rem",
+              }}
+              data-response-headers
+            ></div>
+          </div>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div>
+        <KeyValueTemplate />
       </div>
+    </div>
+  );
+};
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default MyComponent;
